9f87faf1caf509473e189887d0cd66cfe314b1ca
Tom <tomislav.pticek@outlook.com>
2022-10-20 06:22:16 +0000

    removed comments

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-#Step 1 
 import random
 import assets
-
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
 chosen_word = assets.words[random.randint(0,len(assets.words)-1)]
 chosen_word_guess = []
